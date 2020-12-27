05bdbf1c7c68207d7857297a4d84e2ff7fc4e525
beldmian <beldmian@gmail.com>
2020-12-27 11:33:31 +0000

    Simple first ui

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import { Navbar } from './components/Navbar.jsx'
+import Container from '@material-ui/core/Container'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Container fixed>
+        <h1>Taiga bot</h1>
+      </Container>
     </div>
   );
 }
