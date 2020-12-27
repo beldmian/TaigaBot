c3dd54335ad02e8b7c159b36eeef022b361d1f43
beldmian <beldmian@gmail.com>
2020-12-27 12:13:48 +0000

    First content

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -4,8 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,11 +23,8 @@ export function Navbar() {
     return (
         <AppBar position="static">
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
                 <Typography variant="h6" className={classes.title}>
-                    News
+                    TaigaBot
                 </Typography>
                 <Button color="inherit" href="https://discord.com/api/oauth2/authorize?client_id=780455940566024192&permissions=8&scope=bot">Add to server</Button>
             </Toolbar>
